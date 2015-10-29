eb0442eb92d36f8e91c721048dd841ce5a2caf2e
Austin Weale <austinweale@gmail.com>
2015-10-29 10:10:39 -0700

    first step

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@
 	// Use and $http.get request to get the salary data
 	
 		// Set $scope.data equal to the data returned in your results
+
+var sortApp = angular.module('sortApp', []);
+
+sortApp.controller('mainController', function($scope, $http){
+
+    $scope.sortType = "placeHolder";
+
+    $http.get('data/salaries.json').then(function(response){
+        $scope.data = response.data;
+
+    });
+
+});
+
